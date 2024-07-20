9a075dfea5c39426999f74a0a090d696b41998a7
chavavenkat <chavavenkatarajesh@gmail.com>
2024-07-20 13:47:04 +0530

    first commit

--- 05-variables.sh
+++ 05-variables.sh
@@ -2,10 +2,10 @@
 
 echo "Please enter username::"
 
-read USERNAME #where USERNAME IS variable
+read -s USERNAME #where USERNAME IS variable
 
 echo "Please enter password"
 
-read PASSWORD
+read -s PASSWORD
 
 echo "Username is: $USERNAME, Password is: $PASSWORD"
