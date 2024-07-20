5a7e82a82a8d5d6589250b02c7d016874debc7c8
chavavenkat <chavavenkatarajesh@gmail.com>
2024-07-20 13:30:25 +0530

    first commit

---
+++ 03-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=Ramesh
+PERSON2=Suresh
+
+echo "$PERSON1:: Hello $PERSON2, How are you?"
+echo "$PERSON2:: Hi $PERSON1, I am fine, How are you?"
+echo "$PERSON1:: I am fine too. how is your wokr?"
+echo "$PERSON2:: not bad. I am thinking to upgrade to DevOps"
